6f4421523b3ddb637d4a2577b2bc30a1dc506176
krishnadas-altd <krishnadas.pc@altd.in>
2024-03-06 15:58:14 +0530

    added drizzle studio.

--- src/config/drizzle.config.ts
+++ src/config/drizzle.config.ts
@@ -2,4 +2,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/schema/schema.ts",
   out: "./drizzle",
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: `postgres://${process.env.DB_USERNAME}:${process.env.DB_USERNAME}@localhost:5432/${process.env.DB_NAME}`,
+  }
 } satisfies Config;
