984b54b91d6e75738a46e9cd968695cc72335d45
André Le Blanc <leblanc_112@hotmail.com>
2024-03-10 23:06:21 +0100

    added linter and expanded tsconfig

--- src/routes/roles/roles.ts
+++ src/routes/roles/roles.ts
@@ -1,107 +1,144 @@
-import { FastifyPluginAsync } from "fastify"
-import { createRole, getRolesPaginate, getRoleById, updateRoleById, deleteRole, getRoleWithPermissions } from "../../services/roleService";
-import { CreateRoleSchema, CreateRoleSchemaType, ListRoleQueryParamSchema, ListRoleQueryParamSchemaType, PatchRoleSchema, PatchRoleSchemaType, getRoleByIdSchema, getRoleByIdType } from "./roleSchema";
+import { FastifyPluginAsync } from "fastify";
+import {
+  createRole,
+  getRolesPaginate,
+  getRoleById,
+  updateRoleById,
+  deleteRole,
+  getRoleWithPermissions,
+} from "../../services/roleService";
+import {
+  CreateRoleSchema,
+  CreateRoleSchemaType,
+  ListRoleQueryParamSchema,
+  ListRoleQueryParamSchemaType,
+  PatchRoleSchema,
+  PatchRoleSchemaType,
+  getRoleByIdSchema,
+  getRoleByIdType,
+} from "./roleSchema";
 
 const roles: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get<{Querystring: ListRoleQueryParamSchemaType}>('/', 
-  {
-    onRequest: [fastify.authenticate],
-    schema: {
+  fastify.get<{ Querystring: ListRoleQueryParamSchemaType }>(
+    "/",
+    {
+      onRequest: [fastify.authenticate],
+      schema: {
         querystring: ListRoleQueryParamSchema,
+      },
     },
-  },
-  async function (request, reply) {
-    let {search='', limit=10, page=1} = request.query;
-    const offset = fastify.findOffset(limit, page)
-    const result = await getRolesPaginate(search, limit, page, offset);
-    let message: string = fastify.responseMessage("roles", result.data.length)
-    let requestUrl: string | null = request.protocol + '://' + request.hostname + request.url;
-    const nextUrl: string | null = fastify.findNextPageUrl(requestUrl, result.totalPage, page);
-    const previousUrl: string | null = fastify.findPreviousPageUrl(requestUrl, result.totalPage, page);
+    async function (request, reply) {
+      let { search = "", limit = 10, page = 1 } = request.query;
+      const offset = fastify.findOffset(limit, page);
+      const result = await getRolesPaginate(search, limit, page, offset);
+      let message: string = fastify.responseMessage(
+        "roles",
+        result.data.length
+      );
+      let requestUrl: string | null =
+        request.protocol + "://" + request.hostname + request.url;
+      const nextUrl: string | null = fastify.findNextPageUrl(
+        requestUrl,
+        result.totalPage,
+        page
+      );
+      const previousUrl: string | null = fastify.findPreviousPageUrl(
+        requestUrl,
+        result.totalPage,
+        page
+      );
 
-    return { 
-        message: message, 
-        totalItems:  result.totalItems, 
-        nextUrl:nextUrl, 
-        previousUrl: previousUrl, 
-        totalPage: result.totalPage, 
-        page: page, 
-        limit: limit, 
-        data: result.data 
+      return {
+        message: message,
+        totalItems: result.totalItems,
+        nextUrl: nextUrl,
+        previousUrl: previousUrl,
+        totalPage: result.totalPage,
+        page: page,
+        limit: limit,
+        data: result.data,
+      };
     }
-  })
-  fastify.post<{ Body: CreateRoleSchemaType, Reply: object }>(
-    '/',
+  );
+  fastify.post<{ Body: CreateRoleSchemaType; Reply: object }>(
+    "/",
     {
       onRequest: [fastify.authenticate],
       schema: {
-      body: CreateRoleSchema,
-        response: {
-        },
-      }
+        body: CreateRoleSchema,
+        response: {},
+      },
     },
-     async (request, reply) => {
-      const { roleName, description='' } = request.body;
+    async (request, reply) => {
+      const { roleName, description = "" } = request.body;
       const role = await createRole(roleName, description);
-      reply.status(201).send({message: "Role created", data: role});
-  })
-  fastify.get<{Params: getRoleByIdType }>(
-    '/:id',
+      reply.status(201).send({ message: "Role created", data: role });
+    }
+  );
+  fastify.get<{ Params: getRoleByIdType }>(
+    "/:id",
     {
       onRequest: [fastify.authenticate],
       schema: {
-        params: getRoleByIdSchema
-      }
+        params: getRoleByIdSchema,
+      },
     },
-     async (request, reply) => {
-      const id  = request.params.id;
+    async (request, reply) => {
+      const id = request.params.id;
       const role = await getRoleById(id);
-      if(role == null) {
-        return reply.status(404).send({message: "role not found"});
+      if (role == null) {
+        return reply.status(404).send({ message: "role not found" });
       }
       const rolePermissions = await getRoleWithPermissions(role.id);
-      reply.status(200).send({role: role, permissions: rolePermissions});
-  })
-  fastify.patch<{ Body: PatchRoleSchemaType, Reply: object, Params: getRoleByIdType }>(
-    '/:id',
+      reply.status(200).send({ role: role, permissions: rolePermissions });
+    }
+  );
+  fastify.patch<{
+    Body: PatchRoleSchemaType;
+    Reply: object;
+    Params: getRoleByIdType;
+  }>(
+    "/:id",
     {
       onRequest: [fastify.authenticate],
       schema: {
         body: PatchRoleSchema,
-        params: getRoleByIdSchema
-      }
+        params: getRoleByIdSchema,
+      },
     },
-     async (request, reply) => {
+    async (request, reply) => {
       const roleData = request.body;
-      if(Object.keys(roleData).length == 0){
-        return reply.status(422).send({message: "Provide at least one column to update."});
+      if (Object.keys(roleData).length == 0) {
+        return reply
+          .status(422)
+          .send({ message: "Provide at least one column to update." });
       }
-      const id  = request.params.id;
-      
+      const id = request.params.id;
+
       const role = await updateRoleById(id, roleData);
-      if(role.length == 0) {
-        return reply.status(404).send({message: "role not found"});
+      if (role.length == 0) {
+        return reply.status(404).send({ message: "role not found" });
       }
-      reply.status(201).send({message: "Role Updated", data: role});
-  })
-  fastify.delete<{Params: getRoleByIdType }>(
-    '/:id',
+      reply.status(201).send({ message: "Role Updated", data: role });
+    }
+  );
+  fastify.delete<{ Params: getRoleByIdType }>(
+    "/:id",
     {
       onRequest: [fastify.authenticate],
       schema: {
-        params: getRoleByIdSchema
-      }
+        params: getRoleByIdSchema,
+      },
     },
-     async (request, reply) => {
-      const id  = request.params.id;
+    async (request, reply) => {
+      const id = request.params.id;
       const deletedRole = await deleteRole(id);
-      if(deletedRole == null) {
-        return reply.status(404).send({message: "Role doesn't exist!"})
+      if (deletedRole == null) {
+        return reply.status(404).send({ message: "Role doesn't exist!" });
       }
-      return reply.status(200).send({message: "Role deleted"});
-      
-  })
-}
+      return reply.status(200).send({ message: "Role deleted" });
+    }
+  );
+};
 
 export default roles;
-
