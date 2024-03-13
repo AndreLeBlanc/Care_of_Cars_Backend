0bfb00740654417cc46dd1dc55839988da02046a
krishnadas-altd <krishnadas.pc@altd.in>
2024-03-13 10:06:30 +0530

    added types to fastify request instance, removed unused opts.

--- src/routes/serviceCategory/serviceCategory.ts
+++ src/routes/serviceCategory/serviceCategory.ts
@@ -1,47 +1,35 @@
-import { FastifyPluginAsync } from "fastify";
-import {
-  createserviceCategory,
-  getserviceCategoriesPaginate,
-}
-from "../../services/serviceCategory";
+import { FastifyInstance, FastifyPluginAsync } from 'fastify'
+import { createServiceCategory, getServiceCategoriesPaginate } from '../../services/serviceCategory'
 import {
   CreateServiceCategorySchema,
   CreateServiceCategorySchemaType,
   ListServiceCategoryQueryParamSchema,
   ListServiceCategoryQueryParamSchemaType,
-} from "./serviceCategorySchema";
+} from './serviceCategorySchema'
 
-const serviceCategories: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+const serviceCategories: FastifyPluginAsync = async (fastify: FastifyInstance): Promise<void> => {
   fastify.get<{ Querystring: ListServiceCategoryQueryParamSchemaType }>(
-    "/",
+    '/',
     {
-     // onRequest: [fastify.authenticate],
+      // onRequest: [fastify.authenticate],
       schema: {
         querystring: ListServiceCategoryQueryParamSchema,
       },
     },
     async function (request, reply) {
-      let { search = "", limit = 10, page = 1 } = request.query;
-      const offset = fastify.findOffset(limit, page);
-      const result = await getserviceCategoriesPaginate(search, limit, page, offset);
-      let message: string = fastify.responseMessage(
-        "service category",
-        result.data.length
-      );
-      let requestUrl: string | null =
-        request.protocol + "://" + request.hostname + request.url;
-      const nextUrl: string | null = fastify.findNextPageUrl(
-        requestUrl,
-        result.totalPage,
-        page
-      );
+      let { search = '', limit = 10, page = 1 } = request.query
+      const offset = fastify.findOffset(limit, page)
+      const result = await getServiceCategoriesPaginate(search, limit, page, offset)
+      let message: string = fastify.responseMessage('service category', result.data.length)
+      let requestUrl: string | null = request.protocol + '://' + request.hostname + request.url
+      const nextUrl: string | null = fastify.findNextPageUrl(requestUrl, result.totalPage, page)
       const previousUrl: string | null = fastify.findPreviousPageUrl(
         requestUrl,
         result.totalPage,
-        page
-      );
+        page,
+      )
 
-      return {
+      return reply.status(200).send({
         message: message,
         totalItems: result.totalItems,
         nextUrl: nextUrl,
@@ -50,11 +38,11 @@ const serviceCategories: FastifyPluginAsync = async (fastify, opts): Promise<voi
         page: page,
         limit: limit,
         data: result.data,
-      };
-    }
-  );
+      })
+    },
+  )
   fastify.post<{ Body: CreateServiceCategorySchemaType; Reply: object }>(
-    "/",
+    '/',
     {
       //onRequest: [fastify.authenticate],
       schema: {
@@ -63,10 +51,10 @@ const serviceCategories: FastifyPluginAsync = async (fastify, opts): Promise<voi
       },
     },
     async (request, reply) => {
-      const { name, description = "" } = request.body;
-      const serviceCategory = await createserviceCategory(name, description);
-      reply.status(201).send({ message: "Service created", data: serviceCategory });
-    }
-  );
-};
-export default serviceCategories;
+      const { name, description = '' } = request.body
+      const serviceCategory = await createServiceCategory(name, description)
+      reply.status(201).send({ message: 'Service created', data: serviceCategory })
+    },
+  )
+}
+export default serviceCategories
