cdc4338e43e2fd40dc22fc5b2f99e1fa5a69b04d
André Le Blanc <leblanc_112@hotmail.com>
2024-04-27 15:11:04 +0200

    testing comit hook

--- src/services/storeService.ts
+++ src/services/storeService.ts
@@ -91,8 +91,7 @@ export type FromDate = Brand<Date, 'fromDate'>
 export const FromDate = make<FromDate>()
 export type ToDate = Brand<Date, 'toDate'>
 export const ToDate = make<ToDate>()
-{
-}
+
 export type StoreSpecialHours = {
   storeID: StoreID
   specialDateID: SpecialDateID
