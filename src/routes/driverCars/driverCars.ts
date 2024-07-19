c1100aa64dfc510058eeaabd69111c5980d07030
André Le Blanc <leblanc_112@hotmail.com>
2024-07-19 19:20:51 +0200

    refactored driverCars

--- src/routes/driverCars/driverCars.ts
+++ src/routes/driverCars/driverCars.ts
@@ -43,6 +43,8 @@ import {
 
 import { Limit, Offset, Page, Search } from '../../plugins/pagination.js'
 
+import { Either, match } from '../../utils/helper.js'
+
 export const driverCars = async (fastify: FastifyInstance) => {
   fastify.put<{
     Body: DriverCarSchemaType
@@ -78,17 +80,22 @@ export const driverCars = async (fastify: FastifyInstance) => {
         driverCarNotes: DriverCarNotes(req.body.driverCarNotes),
       }
 
-      const car: Car | undefined = await putCar(newCar, driverCarID)
+      const car: Either<string, Car> = await putCar(newCar, driverCarID)
 
-      if (car != null) {
-        return rep.status(201).send({
-          message: 'driver car created/modified successfully',
-          ...car.carInfo,
-          createdAt: car.dates.createdAt.toISOString(),
-          updatedAt: car.dates.updatedAt.toISOString(),
-        })
-      }
-      return rep.status(504).send({ message: 'couldnt create or modify driver car' })
+      match(
+        car,
+        (newCar: Car) => {
+          return rep.status(201).send({
+            message: 'driver car created/modified successfully',
+            ...newCar.carInfo,
+            createdAt: newCar.dates.createdAt.toISOString(),
+            updatedAt: newCar.dates.updatedAt.toISOString(),
+          })
+        },
+        (err) => {
+          return rep.status(504).send({ message: err })
+        },
+      )
     },
   )
 
@@ -113,16 +120,21 @@ export const driverCars = async (fastify: FastifyInstance) => {
       },
     },
     async (request, reply) => {
-      const fetchedCar: Car | undefined = await getCar(DriverCarID(request.params.driverCarID))
-      if (fetchedCar == null) {
-        return reply.status(404).send({ message: "driver car doesn't exist!" })
-      }
-      return reply.status(200).send({
-        message: 'car fetched',
-        ...fetchedCar.carInfo,
-        createdAt: fetchedCar.dates.createdAt.toISOString(),
-        updatedAt: fetchedCar.dates.updatedAt.toISOString(),
-      })
+      const fetchedCar: Either<string, Car> = await getCar(DriverCarID(request.params.driverCarID))
+      match(
+        fetchedCar,
+        (car: Car) => {
+          return reply.status(200).send({
+            message: 'car fetched',
+            ...car.carInfo,
+            createdAt: car.dates.createdAt.toISOString(),
+            updatedAt: car.dates.updatedAt.toISOString(),
+          })
+        },
+        (err) => {
+          return reply.status(404).send({ message: err })
+        },
+      )
     },
   )
 
@@ -147,18 +159,23 @@ export const driverCars = async (fastify: FastifyInstance) => {
       },
     },
     async (request, reply) => {
-      const fetchedCar: Car | undefined = await getCarByReg(
+      const fetchedCar: Either<string, Car> = await getCarByReg(
         DriverCarRegistrationNumber(request.params.driverCarReg),
       )
-      if (fetchedCar == null) {
-        return reply.status(404).send({ message: "driver car doesn't exist!" })
-      }
-      return reply.status(200).send({
-        message: 'car fetched',
-        ...fetchedCar.carInfo,
-        createdAt: fetchedCar.dates.createdAt.toISOString(),
-        updatedAt: fetchedCar.dates.updatedAt.toISOString(),
-      })
+      match(
+        fetchedCar,
+        (car: Car) => {
+          return reply.status(200).send({
+            message: 'car fetched',
+            ...car.carInfo,
+            createdAt: car.dates.createdAt.toISOString(),
+            updatedAt: car.dates.updatedAt.toISOString(),
+          })
+        },
+        (err) => {
+          return reply.status(404).send({ message: err })
+        },
+      )
     },
   )
 
@@ -183,16 +200,23 @@ export const driverCars = async (fastify: FastifyInstance) => {
       },
     },
     async (request, reply) => {
-      const deletedCar: Car | undefined = await deleteCar(DriverCarID(request.params.driverCarID))
-      if (deletedCar == null) {
-        return reply.status(404).send({ message: "driver car doesn't exist!" })
-      }
-      return reply.status(200).send({
-        message: 'car deleted',
-        ...deletedCar.carInfo,
-        createdAt: deletedCar.dates.createdAt.toISOString(),
-        updatedAt: deletedCar.dates.updatedAt.toISOString(),
-      })
+      const deletedCar: Either<string, Car> = await deleteCar(
+        DriverCarID(request.params.driverCarID),
+      )
+      match(
+        deletedCar,
+        (car: Car) => {
+          return reply.status(200).send({
+            message: 'car fetched',
+            ...car.carInfo,
+            createdAt: car.dates.createdAt.toISOString(),
+            updatedAt: car.dates.updatedAt.toISOString(),
+          })
+        },
+        (err) => {
+          return reply.status(404).send({ message: err })
+        },
+      )
     },
   )
 
@@ -224,20 +248,24 @@ export const driverCars = async (fastify: FastifyInstance) => {
       const brandedLimit = limit ? Limit(limit) : undefined
       const brandedPage = page ? Page(page) : undefined
       const brandedOffset = offset ? Offset(offset) : undefined
-      const cars: CarsPaginated | undefined = await getCarsPaginated(
+      const cars: Either<string, CarsPaginated> = await getCarsPaginated(
         brandedSearch,
         brandedLimit,
         brandedOffset,
         brandedPage,
       )
-      if (cars != null) {
-        return rep.status(200).send({
-          message: 'driver cars',
-          ...cars,
-        })
-      } else {
-        rep.status(403).send({ message: 'could list driver cars' })
-      }
+      match(
+        cars,
+        (carsList: CarsPaginated) => {
+          return rep.status(200).send({
+            message: 'driver cars',
+            ...carsList,
+          })
+        },
+        (err) => {
+          rep.status(403).send({ message: err })
+        },
+      )
     },
   )
 }
--- src/services/driverCarService.ts
+++ src/services/driverCarService.ts
@@ -17,6 +17,8 @@ import { and, eq, ilike, or, sql } from 'drizzle-orm'
 
 import { Limit, Offset, Page, ResultCount, Search } from '../plugins/pagination.js'
 
+import { Either, errorHandling, left, right } from '../utils/helper.js'
+
 export type CreateCar = {
   driverID?: DriverID
   driverCarRegistrationNumber: DriverCarRegistrationNumber
@@ -43,107 +45,135 @@ export type CarsPaginated = {
   })[]
 }
 
-export async function putCar(car: CreateCar, driverCarID?: DriverCarID): Promise<Car | undefined> {
-  const [createdCar] = driverCarID
-    ? await db
-        .update(driverCars)
-        .set(car)
-        .where(eq(driverCars.driverCarID, driverCarID))
-        .returning()
-    : await db.insert(driverCars).values(car).returning()
+export async function putCar(
+  car: CreateCar,
+  driverCarID?: DriverCarID,
+): Promise<Either<string, Car>> {
+  try {
+    const [createdCar] = driverCarID
+      ? await db
+          .update(driverCars)
+          .set(car)
+          .where(eq(driverCars.driverCarID, driverCarID))
+          .returning()
+      : await db.insert(driverCars).values(car).returning()
 
-  return {
-    carInfo: {
-      driverCarID: createdCar.driverCarID,
-      driverCarBrand: createdCar.driverCarBrand ?? undefined,
-      driverCarChassiNumber: createdCar.driverCarChassiNumber ?? undefined,
-      driverCarColor: createdCar.driverCarColor ?? undefined,
-      driverCarModel: createdCar.driverCarModel ?? undefined,
-      driverCarNotes: createdCar.driverCarNotes ?? undefined,
-      driverCarRegistrationNumber: createdCar.driverCarRegistrationNumber,
-      driverCarYear: createdCar.driverCarYear ?? undefined,
-      driverID: createdCar.driverID ?? undefined,
-    },
-    dates: {
-      createdAt: createdCar.createdAt,
-      updatedAt: createdCar.updatedAt,
-    },
+    return createdCar
+      ? right({
+          carInfo: {
+            driverCarID: createdCar.driverCarID,
+            driverCarBrand: createdCar.driverCarBrand ?? undefined,
+            driverCarChassiNumber: createdCar.driverCarChassiNumber ?? undefined,
+            driverCarColor: createdCar.driverCarColor ?? undefined,
+            driverCarModel: createdCar.driverCarModel ?? undefined,
+            driverCarNotes: createdCar.driverCarNotes ?? undefined,
+            driverCarRegistrationNumber: createdCar.driverCarRegistrationNumber,
+            driverCarYear: createdCar.driverCarYear ?? undefined,
+            driverID: createdCar.driverID ?? undefined,
+          },
+          dates: {
+            createdAt: createdCar.createdAt,
+            updatedAt: createdCar.updatedAt,
+          },
+        })
+      : left("couldn't create car")
+  } catch (e) {
+    return left(errorHandling(e))
   }
 }
 
-export async function deleteCar(driverCarID: DriverCarID): Promise<Car | undefined> {
-  const [deletedCar] = await db
-    .delete(driverCars)
-    .where(eq(driverCars.driverCarID, driverCarID))
-    .returning()
+export async function deleteCar(driverCarID: DriverCarID): Promise<Either<string, Car>> {
+  try {
+    const [deletedCar] = await db
+      .delete(driverCars)
+      .where(eq(driverCars.driverCarID, driverCarID))
+      .returning()
 
-  return {
-    carInfo: {
-      driverCarID: deletedCar.driverCarID,
-      driverCarBrand: deletedCar.driverCarBrand ?? undefined,
-      driverCarChassiNumber: deletedCar.driverCarChassiNumber ?? undefined,
-      driverCarColor: deletedCar.driverCarColor ?? undefined,
-      driverCarModel: deletedCar.driverCarModel ?? undefined,
-      driverCarNotes: deletedCar.driverCarNotes ?? undefined,
-      driverCarRegistrationNumber: deletedCar.driverCarRegistrationNumber,
-      driverCarYear: deletedCar.driverCarYear ?? undefined,
-      driverID: deletedCar.driverID ?? undefined,
-    },
-    dates: {
-      createdAt: deletedCar.createdAt,
-      updatedAt: deletedCar.updatedAt,
-    },
+    return deletedCar
+      ? right({
+          carInfo: {
+            driverCarID: deletedCar.driverCarID,
+            driverCarBrand: deletedCar.driverCarBrand ?? undefined,
+            driverCarChassiNumber: deletedCar.driverCarChassiNumber ?? undefined,
+            driverCarColor: deletedCar.driverCarColor ?? undefined,
+            driverCarModel: deletedCar.driverCarModel ?? undefined,
+            driverCarNotes: deletedCar.driverCarNotes ?? undefined,
+            driverCarRegistrationNumber: deletedCar.driverCarRegistrationNumber,
+            driverCarYear: deletedCar.driverCarYear ?? undefined,
+            driverID: deletedCar.driverID ?? undefined,
+          },
+          dates: {
+            createdAt: deletedCar.createdAt,
+            updatedAt: deletedCar.updatedAt,
+          },
+        })
+      : left("couldn't find car")
+  } catch (e) {
+    return left(errorHandling(e))
   }
 }
 
-export async function getCar(driverCarID: DriverCarID): Promise<Car | undefined> {
-  const [deletedCar] = await db
-    .select()
-    .from(driverCars)
-    .where(eq(driverCars.driverCarID, driverCarID))
+export async function getCar(driverCarID: DriverCarID): Promise<Either<string, Car>> {
+  try {
+    const [fetchedCar] = await db
+      .select()
+      .from(driverCars)
+      .where(eq(driverCars.driverCarID, driverCarID))
 
-  return {
-    carInfo: {
-      driverCarID: deletedCar.driverCarID,
-      driverCarBrand: deletedCar.driverCarBrand ?? undefined,
-      driverCarChassiNumber: deletedCar.driverCarChassiNumber ?? undefined,
-      driverCarColor: deletedCar.driverCarColor ?? undefined,
-      driverCarModel: deletedCar.driverCarModel ?? undefined,
-      driverCarNotes: deletedCar.driverCarNotes ?? undefined,
-      driverCarRegistrationNumber: deletedCar.driverCarRegistrationNumber,
-      driverCarYear: deletedCar.driverCarYear ?? undefined,
-      driverID: deletedCar.driverID ?? undefined,
-    },
-    dates: {
-      createdAt: deletedCar.createdAt,
-      updatedAt: deletedCar.updatedAt,
-    },
+    return fetchedCar
+      ? right({
+          carInfo: {
+            driverCarID: fetchedCar.driverCarID,
+            driverCarBrand: fetchedCar.driverCarBrand ?? undefined,
+            driverCarChassiNumber: fetchedCar.driverCarChassiNumber ?? undefined,
+            driverCarColor: fetchedCar.driverCarColor ?? undefined,
+            driverCarModel: fetchedCar.driverCarModel ?? undefined,
+            driverCarNotes: fetchedCar.driverCarNotes ?? undefined,
+            driverCarRegistrationNumber: fetchedCar.driverCarRegistrationNumber,
+            driverCarYear: fetchedCar.driverCarYear ?? undefined,
+            driverID: fetchedCar.driverID ?? undefined,
+          },
+          dates: {
+            createdAt: fetchedCar.createdAt,
+            updatedAt: fetchedCar.updatedAt,
+          },
+        })
+      : left("couln't find car")
+  } catch (e) {
+    return left(errorHandling(e))
   }
 }
+
 export async function getCarByReg(
   driverCarReg: DriverCarRegistrationNumber,
-): Promise<Car | undefined> {
-  const [deletedCar] = await db
-    .select()
-    .from(driverCars)
-    .where(eq(driverCars.driverCarRegistrationNumber, driverCarReg))
+): Promise<Either<string, Car>> {
+  try {
+    const [fetchedCar] = await db
+      .select()
+      .from(driverCars)
+      .where(eq(driverCars.driverCarRegistrationNumber, driverCarReg))
 
-  return {
-    carInfo: {
-      driverCarID: deletedCar.driverCarID,
-      driverCarBrand: deletedCar.driverCarBrand ?? undefined,
-      driverCarChassiNumber: deletedCar.driverCarChassiNumber ?? undefined,
-      driverCarColor: deletedCar.driverCarColor ?? undefined,
-      driverCarModel: deletedCar.driverCarModel ?? undefined,
-      driverCarNotes: deletedCar.driverCarNotes ?? undefined,
-      driverCarRegistrationNumber: deletedCar.driverCarRegistrationNumber,
-      driverCarYear: deletedCar.driverCarYear ?? undefined,
-      driverID: deletedCar.driverID ?? undefined,
-    },
-    dates: {
-      createdAt: deletedCar.createdAt,
-      updatedAt: deletedCar.updatedAt,
-    },
+    return fetchedCar
+      ? right({
+          carInfo: {
+            driverCarID: fetchedCar.driverCarID,
+            driverCarBrand: fetchedCar.driverCarBrand ?? undefined,
+            driverCarChassiNumber: fetchedCar.driverCarChassiNumber ?? undefined,
+            driverCarColor: fetchedCar.driverCarColor ?? undefined,
+            driverCarModel: fetchedCar.driverCarModel ?? undefined,
+            driverCarNotes: fetchedCar.driverCarNotes ?? undefined,
+            driverCarRegistrationNumber: fetchedCar.driverCarRegistrationNumber,
+            driverCarYear: fetchedCar.driverCarYear ?? undefined,
+            driverID: fetchedCar.driverID ?? undefined,
+          },
+          dates: {
+            createdAt: fetchedCar.createdAt,
+            updatedAt: fetchedCar.updatedAt,
+          },
+        })
+      : left("couln't find car")
+  } catch (e) {
+    return left(errorHandling(e))
   }
 }
 
@@ -152,61 +182,65 @@ export async function getCarsPaginated(
   limit = Limit(10),
   offset = Offset(0),
   page = Page(1),
-): Promise<CarsPaginated | undefined> {
-  const condition = and(
-    or(
-      ilike(driverCars.driverCarChassiNumber, '%' + search + '%'),
-      ilike(driverCars.driverCarRegistrationNumber, '%' + search + '%'),
-    ),
-  )
-  const { totalCars, listedCars } = await db.transaction(async (tx) => {
-    const [totalCars] = await tx
-      .select({
-        count: sql`count(*)`.mapWith(Number).as('count'),
-      })
-      .from(driverCars)
-      .where(condition)
+): Promise<Either<string, CarsPaginated>> {
+  try {
+    const condition = and(
+      or(
+        ilike(driverCars.driverCarChassiNumber, '%' + search + '%'),
+        ilike(driverCars.driverCarRegistrationNumber, '%' + search + '%'),
+      ),
+    )
+    const { totalCars, listedCars } = await db.transaction(async (tx) => {
+      const [totalCars] = await tx
+        .select({
+          count: sql`count(*)`.mapWith(Number).as('count'),
+        })
+        .from(driverCars)
+        .where(condition)
 
-    const listedCars = await tx
-      .select({
-        driverCarID: driverCars.driverCarID,
-        driverCarBrand: driverCars.driverCarBrand,
-        driverCarChassiNumber: driverCars.driverCarChassiNumber,
-        driverCarColor: driverCars.driverCarColor,
-        driverCarModel: driverCars.driverCarModel,
-        driverCarNotes: driverCars.driverCarNotes,
-        driverCarRegistrationNumber: driverCars.driverCarRegistrationNumber,
-        driverCarYear: driverCars.driverCarYear,
-        driverID: driverCars.driverID,
-      })
-      .from(driverCars)
-      .where(condition)
-      .limit(limit || 10)
-      .offset(offset || 0)
+      const listedCars = await tx
+        .select({
+          driverCarID: driverCars.driverCarID,
+          driverCarBrand: driverCars.driverCarBrand,
+          driverCarChassiNumber: driverCars.driverCarChassiNumber,
+          driverCarColor: driverCars.driverCarColor,
+          driverCarModel: driverCars.driverCarModel,
+          driverCarNotes: driverCars.driverCarNotes,
+          driverCarRegistrationNumber: driverCars.driverCarRegistrationNumber,
+          driverCarYear: driverCars.driverCarYear,
+          driverID: driverCars.driverID,
+        })
+        .from(driverCars)
+        .where(condition)
+        .limit(limit || 10)
+        .offset(offset || 0)
 
-    return { totalCars, listedCars }
-  })
+      return { totalCars, listedCars }
+    })
 
-  const listCarsUndefined = listedCars.map((car) => {
-    return {
-      driverCarID: car.driverCarID,
-      driverCarBrand: car.driverCarBrand ?? undefined,
-      driverCarChassiNumber: car.driverCarChassiNumber ?? undefined,
-      driverCarColor: car.driverCarColor ?? undefined,
-      driverCarModel: car.driverCarModel ?? undefined,
-      driverCarNotes: car.driverCarNotes ?? undefined,
-      driverCarRegistrationNumber: car.driverCarRegistrationNumber,
-      driverCarYear: car.driverCarYear ?? undefined,
-      driverID: car.driverID ?? undefined,
-    }
-  })
+    const listCarsUndefined = listedCars.map((car) => {
+      return {
+        driverCarID: car.driverCarID,
+        driverCarBrand: car.driverCarBrand ?? undefined,
+        driverCarChassiNumber: car.driverCarChassiNumber ?? undefined,
+        driverCarColor: car.driverCarColor ?? undefined,
+        driverCarModel: car.driverCarModel ?? undefined,
+        driverCarNotes: car.driverCarNotes ?? undefined,
+        driverCarRegistrationNumber: car.driverCarRegistrationNumber,
+        driverCarYear: car.driverCarYear ?? undefined,
+        driverID: car.driverID ?? undefined,
+      }
+    })
 
-  const totalPage = Page(Math.ceil(totalCars.count / limit))
-  return {
-    totalCars: ResultCount(totalCars.count),
-    totalPage: totalPage,
-    perPage: limit,
-    page: page,
-    cars: listCarsUndefined,
+    const totalPage = Page(Math.ceil(totalCars.count / limit))
+    return right({
+      totalCars: ResultCount(totalCars.count),
+      totalPage: totalPage,
+      perPage: limit,
+      page: page,
+      cars: listCarsUndefined,
+    })
+  } catch (e) {
+    return left(errorHandling(e))
   }
 }
