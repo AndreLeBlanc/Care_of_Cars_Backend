4a7c090d2424a6cc47f1cc5a9c3f2dae1c6248e0
André Le Blanc <leblanc_112@hotmail.com>
2024-09-09 10:25:51 +0200

    fixed docker compose

---
+++ src/services/statisticService.ts
@@ -0,0 +1,57 @@
+//import { db } from '../config/db-connect.js'
+//
+//import { and, count, eq, getTableColumns, gte, ilike, lte, or, sql } from 'drizzle-orm'
+//
+//import { Either, errorHandling, jsonAggBuildObject, left, right } from '../utils/helper.js'
+//
+//import {
+//  RentCarBooking,
+//  RentCarBookingReply,
+//  RentCarBookingReplyNoBrand,
+//} from './rentCarService.js'
+//
+//import Dinero, { Currency } from 'dinero.js'
+//
+//import { Limit, Offset, Page, ResultCount, Search } from '../plugins/pagination.js'
+//
+//import {
+//  Amount,
+//  Billed,
+//  Discount,
+//  DriverCarID,
+//  DriverFirstName,
+//  DriverID,
+//  DriverLastName,
+//  EmployeeID,
+//  IsBilled,
+//  OrderID,
+//  OrderNotes,
+//  OrderProductNotes,
+//  OrderStatus,
+//  PickupTime,
+//  ProductCostCurrency,
+//  ProductCostNumber,
+//  ProductDescription,
+//  ProductID,
+//  ServiceCostCurrency,
+//  ServiceCostDinero,
+//  ServiceCostNumber,
+//  ServiceID,
+//  ServiceName,
+//  StoreID,
+//  SubmissionTimeOrder,
+//  VatFree,
+//  WorkDay1,
+//  WorkDay2,
+//  WorkDay3,
+//  WorkDay4,
+//  WorkDay5,
+//  billOrders,
+//  drivers,
+//  orderListing,
+//  orderProducts,
+//  orderStatus,
+//  orders,
+//  rentCarBookings,
+//} from '../schema/schema.js'
+//
