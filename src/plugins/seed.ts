6a3ba9bb3828cca7ff198cf453ea49d0edbee282
André Le Blanc <leblanc_112@hotmail.com>
2024-10-14 23:59:42 +0200

    started on seeding permissions and adding multiple perms in one call

--- src/plugins/seed.ts
+++ src/plugins/seed.ts
@@ -71,146 +71,153 @@ import {
 } from '../schema/schema.js'
 dotenv.config()
 
-export enum seedResult {
+export enum SeedResult {
   Failed,
   WrongConfig,
   Success,
   AlreadySeeded,
 }
 
+//async function seedPermissions(): SeedResult {}
+
+async function seedMockData(superAdminPassword: string, superAdminEmail: string) {
+  const role: Either<string, CreatedRole> = await createRole(
+    RoleName('SuperAdmin'),
+    RoleDescription('Super admin user'),
+  )
+
+  match(
+    role,
+    async (createdRole: CreatedRole) => {
+      // Below two envs are required in plugins/env.ts so it will throw message in console if not added.
+      const passwordHash = await generatePasswordHash(UserPassword(superAdminPassword))
+      const user: Either<string, CreatedUser> = await createUser(
+        UserFirstName('SuperAdmin'),
+        UserLastName('SuperAdmin'),
+        UserEmail(superAdminEmail),
+        passwordHash,
+        RoleID(createdRole.roleID),
+        IsSuperAdmin(true),
+      )
+      console.info('Super admin created from seed!', role, user)
+      const roleSecond: Either<string, CreatedRole> = await createRole(
+        RoleName('testRole'),
+        RoleDescription('second test user'),
+      )
+      const userSecond: Either<string, CreatedUser> = await createUser(
+        UserFirstName('SuperAdmin'),
+        UserLastName('SuperAdmin'),
+        UserEmail('27CM@HOTMAIL.COM'),
+        passwordHash,
+        RoleID(createdRole.roleID),
+        IsSuperAdmin(true),
+      )
+      console.info('Second user created from seed!', roleSecond, userSecond)
+    },
+    (err) => {
+      console.log('seed error: ', err)
+    },
+  )
+
+  const storeInfo: StoreCreate = {
+    storeName: StoreName('Vendstore'),
+    storeOrgNumber: StoreOrgNumber('14771477'),
+    storeStatus: StoreStatus(true),
+    storeEmail: StoreEmail('andre@vendfox.com'),
+    storePhone: StorePhone('0762757764'),
+    storeAddress: StoreAddress('Norrtäljegatan 15a'),
+    storeZipCode: StoreZipCode('75327'),
+    storeCity: StoreCity('Uppsala'),
+    storeCountry: StoreCountry('Sverige'),
+    storeDescription: StoreDescription('Discount web agency'),
+    storeContactPerson: StoreContactPerson('Blanken'),
+    storeMaxUsers: StoreMaxUsers(400),
+    storeAllowCarAPI: StoreAllowCarAPI(true),
+    storeAllowSendSMS: StoreAllowSendSMS(true),
+    storeSendSMS: StoreSendSMS(true),
+    storeUsesCheckin: StoreUsesCheckin(true),
+    storeUsesPIN: StoreUsesPIN(true),
+    storeWebSite: StoreWebSite('vendfox.com'),
+    storeVatNumber: StoreVatNumber('123123123'),
+    storeFSkatt: StoreFSkatt(true),
+  }
+
+  const pay: StorePaymentOptions = {
+    bankgiro: StoreBankgiro('2342442123'),
+    paymentdays: StorePaymentdays(30),
+  }
+
+  const store: Either<
+    string,
+    {
+      store: Store
+      paymentInfo: StorePaymentOptions | undefined
+      updatedAt: Date
+      createdAt: Date
+    }
+  > = await createStore(storeInfo, pay)
+  console.log('created store status: ', store)
+
+  const catService: Either<string, CreateServiceCategory> = await createServiceCategory(
+    ServiceCategoryName('seeded service'),
+    ServiceCategoryDescription('service created during seed'),
+  )
+  console.log('created service category status: ', catService)
+
+  const catProduct: Either<string, CreateProductCategory> = await createProductCategory(
+    ProductCategoryName('seeded products'),
+    ProductCategoryDescription('Products created during seed'),
+  )
+  console.log('created product category status: ', catProduct)
+
+  match(
+    catService,
+    async (cat: CreateServiceCategory) => {
+      const newService: ServiceCreate = {
+        name: ServiceName('init service'),
+        cost: ServiceCostDinero(Dinero({ amount: 100, currency: 'SEK' })),
+        award: Award(0.01),
+        serviceCategoryID: ServiceCategoryID(cat.serviceCategoryID),
+        includeInAutomaticSms: ServiceIncludeInAutomaticSms(true),
+        hidden: ServiceHidden(false),
+        callInterval: ServiceCallInterval(180),
+        colorForService: 'None',
+        warrantyCard: ServiceWarrantyCard(true),
+        itemNumber: ServiceItemNumber('sdf'),
+        suppliersArticleNumber: ServiceSuppliersArticleNumber('2342'),
+        externalArticleNumber: ServiceExternalArticleNumber('asdf'),
+        day1: ServiceDay1('01:30:00'),
+        day2: ServiceDay2('01:30:00'),
+        day3: ServiceDay3('01:30:00'),
+        day4: ServiceDay4('01:30:00'),
+        day5: ServiceDay5('01:30:00'),
+        serviceVariants: [],
+      }
+
+      const service: Either<string, Service> = await createService(newService, [])
+      console.log('created service status: ', service)
+    },
+    (err: string) => {
+      console.log("couldn't create cat: ", err)
+    },
+  )
+}
+
 export default fp(async () => {
-  async function seedSuperAdmin(): Promise<seedResult> {
+  async function seedSuperAdmin(): Promise<SeedResult> {
     const superAdminPassword = process.env.SUPER_ADMIN_PASSWORD
     const superAdminEmail = process.env.SUPER_ADMIN_EMAIL
     try {
+      //      const seedPermsRes = seedPermissions()
       if (typeof superAdminPassword === 'string' && typeof superAdminEmail === 'string') {
-        const role: Either<string, CreatedRole> = await createRole(
-          RoleName('SuperAdmin'),
-          RoleDescription('Super admin user'),
-        )
-
-        match(
-          role,
-          async (createdRole: CreatedRole) => {
-            // Below two envs are required in plugins/env.ts so it will throw message in console if not added.
-            const passwordHash = await generatePasswordHash(UserPassword(superAdminPassword))
-            const user: Either<string, CreatedUser> = await createUser(
-              UserFirstName('SuperAdmin'),
-              UserLastName('SuperAdmin'),
-              UserEmail(superAdminEmail),
-              passwordHash,
-              RoleID(createdRole.roleID),
-              IsSuperAdmin(true),
-            )
-            console.info('Super admin created from seed!', role, user)
-            const roleSecond: Either<string, CreatedRole> = await createRole(
-              RoleName('testRole'),
-              RoleDescription('second test user'),
-            )
-            const userSecond: Either<string, CreatedUser> = await createUser(
-              UserFirstName('SuperAdmin'),
-              UserLastName('SuperAdmin'),
-              UserEmail('27CM@HOTMAIL.COM'),
-              passwordHash,
-              RoleID(createdRole.roleID),
-              IsSuperAdmin(true),
-            )
-            console.info('Second user created from seed!', roleSecond, userSecond)
-          },
-          (err) => {
-            console.log('seed error: ', err)
-          },
-        )
-
-        const storeInfo: StoreCreate = {
-          storeName: StoreName('Vendstore'),
-          storeOrgNumber: StoreOrgNumber('14771477'),
-          storeStatus: StoreStatus(true),
-          storeEmail: StoreEmail('andre@vendfox.com'),
-          storePhone: StorePhone('0762757764'),
-          storeAddress: StoreAddress('Norrtäljegatan 15a'),
-          storeZipCode: StoreZipCode('75327'),
-          storeCity: StoreCity('Uppsala'),
-          storeCountry: StoreCountry('Sverige'),
-          storeDescription: StoreDescription('Discount web agency'),
-          storeContactPerson: StoreContactPerson('Blanken'),
-          storeMaxUsers: StoreMaxUsers(400),
-          storeAllowCarAPI: StoreAllowCarAPI(true),
-          storeAllowSendSMS: StoreAllowSendSMS(true),
-          storeSendSMS: StoreSendSMS(true),
-          storeUsesCheckin: StoreUsesCheckin(true),
-          storeUsesPIN: StoreUsesPIN(true),
-          storeWebSite: StoreWebSite('vendfox.com'),
-          storeVatNumber: StoreVatNumber('123123123'),
-          storeFSkatt: StoreFSkatt(true),
-        }
-
-        const pay: StorePaymentOptions = {
-          bankgiro: StoreBankgiro('2342442123'),
-          paymentdays: StorePaymentdays(30),
-        }
-
-        const store: Either<
-          string,
-          {
-            store: Store
-            paymentInfo: StorePaymentOptions | undefined
-            updatedAt: Date
-            createdAt: Date
-          }
-        > = await createStore(storeInfo, pay)
-        console.log('created store status: ', store)
-
-        const catService: Either<string, CreateServiceCategory> = await createServiceCategory(
-          ServiceCategoryName('seeded service'),
-          ServiceCategoryDescription('service created during seed'),
-        )
-        console.log('created service category status: ', catService)
-
-        const catProduct: Either<string, CreateProductCategory> = await createProductCategory(
-          ProductCategoryName('seeded products'),
-          ProductCategoryDescription('Products created during seed'),
-        )
-        console.log('created product category status: ', catProduct)
-
-        match(
-          catService,
-          async (cat: CreateServiceCategory) => {
-            const newService: ServiceCreate = {
-              name: ServiceName('init service'),
-              cost: ServiceCostDinero(Dinero({ amount: 100, currency: 'SEK' })),
-              award: Award(0.01),
-              serviceCategoryID: ServiceCategoryID(cat.serviceCategoryID),
-              includeInAutomaticSms: ServiceIncludeInAutomaticSms(true),
-              hidden: ServiceHidden(false),
-              callInterval: ServiceCallInterval(180),
-              colorForService: 'None',
-              warrantyCard: ServiceWarrantyCard(true),
-              itemNumber: ServiceItemNumber('sdf'),
-              suppliersArticleNumber: ServiceSuppliersArticleNumber('2342'),
-              externalArticleNumber: ServiceExternalArticleNumber('asdf'),
-              day1: ServiceDay1('01:30:00'),
-              day2: ServiceDay2('01:30:00'),
-              day3: ServiceDay3('01:30:00'),
-              day4: ServiceDay4('01:30:00'),
-              day5: ServiceDay5('01:30:00'),
-              serviceVariants: [],
-            }
-
-            const service: Either<string, Service> = await createService(newService, [])
-            console.log('created service status: ', service)
-          },
-          (err: string) => {
-            console.log("couldn't create cat: ", err)
-          },
-        )
-        return seedResult.Success
+        seedMockData(superAdminPassword, superAdminEmail)
+        return SeedResult.Success
       } else {
-        return seedResult.WrongConfig
+        return SeedResult.WrongConfig
       }
     } catch (err: unknown) {
       console.error(err)
-      return seedResult.Failed
+      return SeedResult.Failed
     }
   }
   if (process.env.RUN_SEED === 'true') {
@@ -220,6 +227,6 @@ export default fp(async () => {
 
 declare module 'fastify' {
   export interface FastifyInstance {
-    seedSuperAdmin(): Promise<seedResult>
+    seedSuperAdmin(): Promise<SeedResult>
   }
 }
--- src/routes/permissions/permissionSchema.ts
+++ src/routes/permissions/permissionSchema.ts
@@ -1,26 +1,48 @@
 import { Static, Type } from '@sinclair/typebox'
 
 export const PermissionID = Type.Integer()
+const PermissionName = Type.String({ minLength: 3, maxLength: 256 })
+const PermissionDescription = Type.String()
+const MessageSchema = Type.String()
+
+export const MessageReplySchema = Type.Object({ message: MessageSchema })
+
+export type MessageReplySchemaType = Static<typeof MessageReplySchema>
 
 export const ListPermissionQueryParamSchema = Type.Object({
   search: Type.Optional(Type.String()),
   limit: Type.Optional(Type.Integer({ minimum: 1, default: 10 })),
   page: Type.Optional(Type.Integer({ minimum: 1, default: 1 })),
 })
+
 export type ListPermissionQueryParamSchemaType = Static<typeof ListPermissionQueryParamSchema>
 
-const PermissionName = Type.String({ minLength: 3, maxLength: 256 })
-const PermissionDescription = Type.String()
+export const CreatePermissionSchema = Type.Array(
+  Type.Object({
+    permissionTitle: PermissionName,
+    description: Type.Optional(PermissionDescription),
+  }),
+)
 
-export const CreatePermissionSchema = Type.Object({
-  permissionTitle: PermissionName,
-  description: Type.Optional(PermissionDescription),
-})
 export type CreatePermissionSchemaType = Static<typeof CreatePermissionSchema>
+
+export const CreatePermissionResponseSchema = Type.Object({
+  message: MessageSchema,
+  permissions: Type.Array(
+    Type.Object({
+      permissionID: PermissionID,
+      permissionTitle: PermissionName,
+      description: Type.Optional(PermissionDescription),
+    }),
+  ),
+})
+
+export type CreatePermissionResponseSchemaType = Static<typeof CreatePermissionResponseSchema>
 
 export const getPermissionByIDSchema = Type.Object({
   permissionID: PermissionID,
 })
+
 export type getPermissionByIDType = Static<typeof getPermissionByIDSchema>
 
 export const PatchPermissionSchema = Type.Object({
